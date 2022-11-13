9959faf406190d91f51de0929ed9845af329b415
YadinaM <yadina.moreira@gmail.com>
2022-11-13 16:46:27 +0100

    opmaak af

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableNativeFeedback } from 'react-native';
 
 import BookTile from './component/BookTile'
 export default function App() {
@@ -7,8 +7,12 @@ export default function App() {
     <ScrollView style={StyleSheet.container}>
          <Text style={styles.header1}>Bookshop</Text>
         <View style={styles.tilesContainer}>
-          <BookTile title="Book Title" subtitlepwd="by some random dude" text="Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw," isbn="ISBN 123456789"/>
-
+          <BookTile title="Book Title" subtitlepwd="by some random dude" text="Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw," isbn="ISBN 123456789" />
+          <BookTile title="Book Title" subtitlepwd="by some random dude" text="Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw," isbn="ISBN 123456789" />
+          <BookTile title="Book Title" subtitlepwd="by some random dude" text="Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw," isbn="ISBN 123456789" />
+          <BookTile title="Book Title" subtitlepwd="by some random dude" text="Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw," isbn="ISBN 123456789" />
+          <BookTile title="Book Title" subtitlepwd="by some random dude" text="Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw," isbn="ISBN 123456789" />
+          
           <StatusBar style="auto"/>
         </View>
     </ScrollView>
@@ -28,6 +32,29 @@ const styles = StyleSheet.create({
     flexShrink: 0,
     justifyContent: 'space-evenly',
 
+  },
+
+  tile: {
+    flexBasis: '46%',//200
+    borderRadius: 5,
+    borderColor: '#B4B8DA',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    marginVertical: '2%',
+  },
+
+  btn:{
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    flexGrow: 0,
+    flexShrink: 0,
+    justifyContent: 'flex-end',
+    borderRadius: 5,
+    borderColor: '#B4B8DA',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    marginVertical: '2%',
   },
  
 
